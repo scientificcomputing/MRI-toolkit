ad7e108b84b228fba696837818e1c97e013743c6
Henrik Finsberg <henriknf@simula.no>
2026-02-09 11:19:13 +0100

    Add importlib metadata

--- src/mritk/__init__.py
+++ src/mritk/__init__.py
@@ -4,7 +4,17 @@ Copyright (C) 2026   Cécile Daversin-Catty (cecile@simula.no)
 Copyright (C) 2026   Simula Research Laboratory
 """
 
+from importlib.metadata import metadata
+
 from . import data, segmentation, statistics
+
+
+meta = metadata("mri-toolkit")
+__version__ = meta["Version"]
+__author__ = meta["Author-email"]
+__license__ = meta["License"]
+__email__ = meta["Author-email"]
+__program_name__ = meta["Name"]
 
 
 __all__ = [
